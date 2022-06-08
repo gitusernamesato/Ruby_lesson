31e3c60bc5d0c4905adecef2cb606d213e781b70
EC2 Default User <shinya.sin09@gmail.com>
2022-06-08 06:27:05 +0000

    [finish]2-6

---
+++ lesson6-2.rb
@@ -0,0 +1,25 @@
+# 2つの整数値の入力を受け付け、
+# それらの四則演算の結果を出力するプログラム
+
+puts "計算をはじめます。"
+puts "何回計算を繰り返しますか？"
+n = gets.to_i
+
+i = 1
+while i <= n do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください。"
+  a = gets.to_i
+  b = gets.to_i
+  puts "a=#{a}"
+  puts "b=#{b}"
+  puts "計算結果を出力します。"
+  puts "a+b=#{a + b}"
+  puts "a-b=#{a - b}"
+  puts "a*b=#{a * b}"
+  puts "a/b=#{a / b}"
+
+  i += 1
+end
+
+puts "計算を終了します。"
